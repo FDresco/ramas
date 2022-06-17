0cc9158988d45f6c13c0743659419cb21b4d9412
Florencia Dresco <florenciadresco@gmail.com>
2022-06-17 12:33:50 -0300

    agregue js y clase h3

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log(document.querySelector("title").innerText)
+
+console.log(document.getElementsByTagName("h1")[0].innerText)
+
+console.log(document.getElementsByTagName("h2")[0])
+
+console.log(document.getElementsByClassName("clase-h3")[0].innerText)
+
+console.log(document.querySelector(".clase-h3").innerText)
